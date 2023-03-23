2c7d4430fff75e52a209a455a31c4c746ad7764c
Alfonso <agponchito@hotmail.com>
2023-03-23 23:41:43 +0100

    Add to modal component

--- src/components/Game/Modal.vue
+++ src/components/Game/Modal.vue
@@ -1,9 +1,15 @@
 <script>
 import Dice from './Dice.vue';
+import { store } from '../../store.js';
 
 export default {
     components: {
         Dice,
+    },
+    data() {
+        return {
+            store,
+        };
     },
 }
 </script>
@@ -11,6 +17,21 @@ export default {
 <template>
     <div class="modal-overlay">
         <div class="modal">
+            <div v-if="store.isOver" class="winner-announcement">
+                <h1>Winner!</h1>
+                <h2 :style="{ color: this.store.currentPlayer.color }">
+                    {{ store.currentPlayer.name }} has reached the goal
+                </h2>
+                <button @click="store.exitGame">Exit Game</button>
+            </div>
+            <div>
+                <h1>You landed on a snake!!</h1>
+                <h2>Roll the dice to see how far you will drop</h2>
+            </div>
+            <div>
+                <h1>You landed on stairs!!</h1>
+                <h2>Roll the dice to see how far you will climb</h2>
+            </div>
             <h1>This is modal</h1>
             <Dice />
         </div>
@@ -35,5 +56,9 @@ export default {
     background-color: white;
     width: 500px;
     height: 500px;
+}
+
+.winner-announcement {
+    text-align: center;
 }
 </style>
